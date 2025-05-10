6de2ec7120a3831ced090de126182c9b6885125e
colander21 <carsonolander10@gmail.com>
2025-05-10 12:03:56 -0700

    working grid layout for container and box pages

--- packages/frontend/src/pages/BoxesPage.jsx
+++ packages/frontend/src/pages/BoxesPage.jsx
@@ -1,5 +1,23 @@
+import "../styles/Grid.css"
+
 function BoxesPage() {
-  return <h1>Here are your boxes</h1>;
+  return (
+    <div>
+      <div>
+        <h1>Here are your boxes</h1>
+      </div>
+      <div class="grid-box-pg">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+    </div>
+  );
 }
 
 export default BoxesPage;
--- packages/frontend/src/pages/ContainersPage.jsx
+++ packages/frontend/src/pages/ContainersPage.jsx
@@ -1,5 +1,23 @@
+import "../styles/Grid.css";
+
 function ContainersPage() {
-  return <h1>Move-N-Stuff</h1>;
+  return (
+    <div>
+      <div>
+        <h1>Move-N-Stuff</h1>
+      </div>
+      <div class="grid-container-pg">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+    </div>
+  );
 }
 
 export default ContainersPage;
